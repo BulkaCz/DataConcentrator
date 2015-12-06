0f56b24c24f8b7cdca7d67a4b36a34757b779ad1
BulkaCz <bulkacv@gmail.com>
2015-12-06 16:35:35 +0100

    Pripojeni k Azure DB

--- DataConcentrator/Program.cs
+++ DataConcentrator/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+//using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+//using System.Threading.Tasks;
 
 namespace DataConcentrator
 {
@@ -10,6 +10,9 @@ namespace DataConcentrator
     {
         static void Main(string[] args)
         {
+            string connectionString = "Server=tcp:cevelsql.database.windows.net,1433; Database=CevelDB; User ID=BulkaCz@cevelsql; Password=Hovnohovno11; Trusted_Connection=False; Encrypt=True; Connection Timeout=10;";
+            //string connectionString = "Server=tcp:cevelsql.database.windows.net,1433; Database=CevelSQL; User ID=BulkaCz@cevelsql; Password=Hovnohovno11; Encrypt=True; TrustServerCertificate=False; Connection Timeout=10;";
+            //string connectionString = "Data Source=DESKTOP-OKII9AL\\SQLEXPRESS;Initial Catalog = CevelSQL; Integrated Security = True; Connection Timeout=3";
             ElektromerDataType elektromer = new ElektromerDataType();
             elektromer.idMericihoBodu = 1;
             elektromer.cas = DateTime.Now;
@@ -18,8 +21,9 @@ namespace DataConcentrator
             elektromer.cinnyVykon = 33.0f;
             elektromer.jalovyVykon = 44.0f;
             elektromer.ucinnik = 0.99f;
-            SendDataToSQL sendDataToSQL = new SendDataToSQL("Data Source=DESKTOP-OKII9AL\\SQLEXPRESS;Initial Catalog = CevelSQL; Integrated Security = True");
-            sendDataToSQL.Send(elektromer);
+            SendDataToSQL sendDataToSQL = new SendDataToSQL(connectionString);
+            int result = sendDataToSQL.Send(elektromer);
+            Console.WriteLine(result.ToString());
             Console.ReadLine();
         }
     }
--- DataConcentrator/SendDataToSQL.cs
+++ DataConcentrator/SendDataToSQL.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+//using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+//using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
 
@@ -18,42 +18,54 @@ namespace DataConcentrator
             connectionString = _connectionString;
         }
 
-        public void Send(ElektromerDataType elektromer)
+        public int Send(ElektromerDataType elektromer)
         {
+            int result = 0;
             Console.WriteLine("Navazuji spojeni ...");
-            SqlConnection sqlConnection = new SqlConnection(connectionString);
-            sqlConnection.Open();
-            Console.WriteLine("Spojeni navazano :) ");
+            try
+            {
+                SqlConnection sqlConnection = new SqlConnection(connectionString);
+                sqlConnection.Open();
+                Console.WriteLine("Spojeni navazano :) ");
 
-            SqlDataAdapter da = new SqlDataAdapter();
-            da.InsertCommand = sqlConnection.CreateCommand();
+                SqlDataAdapter da = new SqlDataAdapter();
+                da.InsertCommand = sqlConnection.CreateCommand();
 
-            da.SelectCommand = sqlConnection.CreateCommand();
-            da.SelectCommand.CommandText = "SELECT * FROM Elektromer";
+                da.SelectCommand = sqlConnection.CreateCommand();
+                da.SelectCommand.CommandText = "SELECT * FROM Elektromer";
 
-            da.InsertCommand.CommandText = "INSERT INTO Elektromer (id, cas, cinnaEnergie, jalovaEnergie, cinnyVykon, jalovyVykon, ucinnik) VALUES (@id, @cas, @cinnaEnergie, @jalovaEnergie, @cinnyVykon, @jalovyVykon, @ucinnik)";
-            da.InsertCommand.Parameters.Add("@id", SqlDbType.Int, 4, "id");
-            da.InsertCommand.Parameters.Add("@cas", SqlDbType.DateTime2, 8, "cas");
-            da.InsertCommand.Parameters.Add("@cinnaEnergie", SqlDbType.Real, 4, "cinnaEnergie");
-            da.InsertCommand.Parameters.Add("@jalovaEnergie", SqlDbType.Real, 4, "jalovaEnergie");
-            da.InsertCommand.Parameters.Add("@cinnyVykon", SqlDbType.Real, 4, "cinnyVykon");
-            da.InsertCommand.Parameters.Add("@jalovyVykon", SqlDbType.Real, 4, "jalovyVykon");
-            da.InsertCommand.Parameters.Add("@ucinnik", SqlDbType.Real, 4, "ucinnik");
+                da.InsertCommand.CommandText = "INSERT INTO Elektromer (id, cas, cinnaEnergie, jalovaEnergie, cinnyVykon, jalovyVykon, ucinnik) VALUES (@id, @cas, @cinnaEnergie, @jalovaEnergie, @cinnyVykon, @jalovyVykon, @ucinnik)";
+                da.InsertCommand.Parameters.Add("@id", SqlDbType.Int, 4, "id");
+                da.InsertCommand.Parameters.Add("@cas", SqlDbType.DateTime2, 8, "cas");
+                da.InsertCommand.Parameters.Add("@cinnaEnergie", SqlDbType.Real, 4, "cinnaEnergie");
+                da.InsertCommand.Parameters.Add("@jalovaEnergie", SqlDbType.Real, 4, "jalovaEnergie");
+                da.InsertCommand.Parameters.Add("@cinnyVykon", SqlDbType.Real, 4, "cinnyVykon");
+                da.InsertCommand.Parameters.Add("@jalovyVykon", SqlDbType.Real, 4, "jalovyVykon");
+                da.InsertCommand.Parameters.Add("@ucinnik", SqlDbType.Real, 4, "ucinnik");
 
-            DataSet ds = new DataSet();
-            da.Fill(ds);
-            sqlConnection.Close();
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                sqlConnection.Close();
 
-            DataRow dr = ds.Tables[0].NewRow();
-            dr["id"] = elektromer.idMericihoBodu;
-            dr["cas"] = DateTime.Now;
-            dr["cinnaEnergie"] = elektromer.cinnaEnergie;
-            dr["jalovaEnergie"] = elektromer.jalovaEnergie;
-            dr["cinnyVykon"] = elektromer.cinnyVykon;
-            dr["jalovyVykon"] = elektromer.jalovyVykon;
-            dr["ucinnik"] = elektromer.ucinnik;
-            ds.Tables[0].Rows.Add(dr);
-            da.Update(ds);
+                DataRow dr = ds.Tables[0].NewRow();
+                dr["id"] = elektromer.idMericihoBodu;
+                dr["cas"] = DateTime.Now;
+                dr["cinnaEnergie"] = elektromer.cinnaEnergie;
+                dr["jalovaEnergie"] = elektromer.jalovaEnergie;
+                dr["cinnyVykon"] = elektromer.cinnyVykon;
+                dr["jalovyVykon"] = elektromer.jalovyVykon;
+                dr["ucinnik"] = elektromer.ucinnik;
+                ds.Tables[0].Rows.Add(dr);
+                da.Update(ds);
+                result = 1;
+            }
+
+            catch(Exception ex)
+            {
+                result = 0;
+            }
+
+            return result;
         }
     }
 }
